d4277b6b0ea54114c1b60c48c64a48886d4424f6
TizianoJX <tizianojx@gmail.com>
2019-08-23 23:29:16 +0200

    First Version

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, fs, process};
+
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Cannot find file -> {}", err);
+        process::exit(-1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+    println!("{}", contents);
+}
